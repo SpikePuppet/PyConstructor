a0dfe5ad6cca75d856af899efff79cfe67eef785
Rhys Johns <rhysjohnsdev@gmail.com>
2022-04-02 21:03:44 +1100

    - We now load YAML file on start - We also print out all of the commands that run on change

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import time
 import sys
+import yaml
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
 class ProjectWatcher(FileSystemEventHandler):
+    commands = []
+
+    def __init__(self, commands):
+        self.commands = commands
+
+    @staticmethod
+    def print_commands():
+        for index in range(0, len(commands)):
+            print(commands[index])
+
     def on_modified(self, event):
         print(f'The file {event.src_path} was modified')
+        self.print_commands()
 
     def on_created(self, event):
         print(f'The file {event.src_path} was created! Nice')
+        self.print_commmands()
 
     def on_deleted(self, event):
         print(f'The file {event.src_path} was created! D:')
+        self.print_commmands()
 
     def on_moved(self, event):
         print(f'The file {event.src_path} was moved? :O')
+        self.print_commmands()
 
     def execute_observer_options(self):
         pass
@@ -26,12 +41,20 @@ def startup_message():
     print('This program will handle running you\'re pre-defined build steps every time you hit save')
 
 
+def read_yaml():
+    with open('sample-observer-config.yml') as fileHandle:
+        data = yaml.load(fileHandle, Loader=yaml.FullLoader)
+
+    return data[0]['observer']['execute']
+
+
 if __name__ == "__main__":
     directory_to_be_monitored = sys.argv[1]
     recursive = sys.argv[2]
     startup_message()
+    commands = read_yaml()
 
-    fileHandler = ProjectWatcher()
+    fileHandler = ProjectWatcher(commands)
     observer = Observer()
     observer.schedule(fileHandler, path=directory_to_be_monitored, recursive=recursive)
     observer.start()

d0f562607da1907d9df30d6a2236edd920e69fcc
Rhys Johns <rhysjohnsdev@gmail.com>
2022-04-02 20:04:58 +1100

    - Added a really simple file watcher, it basically outputs a message when a pre-defined folder does its thing.

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('Welcome to PyConstructor v0.1')
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class ProjectWatcher(FileSystemEventHandler):
+    logger = logging.getLogger('ProjectWatcher')
+    logger.setLevel(level=logging.DEBUG)
+
+    def on_modified(self, event):
+        print(f'The file {event.src_path} was modified')
+
+    def on_created(self, event):
+        print(f'The file {event.src_path} was created! Nice')
+
+    def on_deleted(self, event):
+        print(f'The file {event.src_path} was created! D:')
+
+    def on_moved(self, event):
+        print(f'The file {event.src_path} was moved? :O')
+
+
+def startup_message():
+    print('Welcome to PyConstructor v0.1')
+    print('This program will handle running you\'re pre-defined build steps every time you hit save')
+
+
+if __name__ == "__main__":
+    startup_message()
+
+    fileHandler = ProjectWatcher()
+    observer = Observer()
+    observer.schedule(fileHandler, path='/Users/rhysjohns/Code/Python/PyConstructor/test_dir', recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
